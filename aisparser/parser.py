354d5e283ede40f900643db447fc6f226b61d009
macrovve <macrovve@gmail.com>
2017-05-24 16:20:39 +0800

    init

---
+++ aisparser/parser.py
@@ -0,0 +1,2 @@
+def parse():
+    print('hello world')
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='aisparser',
+    version='0.0.1',
+    keyword=['AIS', 'Automatic Identification System'],
+    description='Python library parsing AIS message',
+    author='Macrovve',
+    author_email='macrovve@gmail.com',
+    license='MIT License',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Multimedia',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ])
